9422a28366098d2463a34e02d1fc90319b372ad9
gauravshegokar93 <gauravshegokar2312@gmail.com>
2024-12-03 09:18:33 +0530

    refactor: add new file string

--- Comment.py
+++ Comment.py
@@ -7,3 +7,9 @@ Comments can be used to prevent execution when testing code.
 
 
 """
+
+import random
+print(random.randrange(1,10))
+
+a = "hello world"
+print(len(a))
---
+++ string.py
@@ -0,0 +1,41 @@
+"""Strings
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function"""
+
+print("hello")
+print('hello')
+
+
+#assign string to a veriable
+
+a = "hello"
+print(a)
+
+
+#multiline string
+
+a = """ loream is dolor sit amet,
+consectectue adupisicing elit"""
+
+print(a)
+
+
+#string are array
+
+a = "hello, world"
+print(a[1])
+
+
+#lopping throught the string
+
+for x in "banana":
+    print(x)
+
+
+#find the string lenght
+
+a = "hello world"
+print(len(a))
